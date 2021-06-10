baad7075847faf14e5ee234a49e0e50741f60cf4
yulai <97269648@qq.com>
2021-06-10 09:15:13 +0800

    '第二个页面'

---
+++ app/src/main/java/com/www/annotation/TwoActivity.java
@@ -0,0 +1,14 @@
+package com.www.annotation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TwoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+    }
+}
